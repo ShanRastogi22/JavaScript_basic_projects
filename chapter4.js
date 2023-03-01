5a0c627596c1f939e3499e43a9eca2c214bdf469
ShanRastogi22 <nash82826@gmail.com>
2023-03-01 06:06:28 +0530

    this is practice set

---
+++ chapter4.js
@@ -0,0 +1,29 @@
+// what is the following print in js 
+
+let a = "shan\'r";
+let b = "shan\rr";
+let c = "shan\nr";
+let d = "shan\tr";
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(a.length)
+console.log(a.length)
+console.log(a.length)
+console.log(a.length)
+
+// example of includes 
+
+const sent = "shan is not a losser";
+const word = "shan";
+console.log(sent.includes(word))
+console.log(`the word "${word}" ${sent.includes(word)? `is` : `is not`} in the sentence`)
+
+// extract the amount out of this string 
+
+
+let str = "please give Rs 1000"
+let am = Number.parseInt(str.slice('15'))
+console.log(am)
+console.log(typeof am)
