8b87cd2986d150cf398b9fa5a548dd96c6451a90
ShanRastogi22 <nash82826@gmail.com>
2023-03-04 11:03:54 +0530

    practice gir modules

---
+++ chapter5.js
@@ -0,0 +1,35 @@
+//  convert an array into a string
+/* let num = [1,2,3,34,];
+ let b = num.toString()
+ console.log(typeof num, num)
+ console.log(typeof b, b)*/
+
+//  pop ex.
+/*let num = [1,2,3,34,];
+let r = num.pop()
+console.log(num, r)
+console.log(typeof num, num)
+console.log(typeof r, r)*/
+
+// example of map method
+/*let arr = [45, 56 , 67];
+ let a = arr.map((value, index, array) =>{
+    console.log(value, index, array)
+    return value + index
+ })
+  console.log(a)*/
+
+//   example of filter method 
+/*let arr = [2,3,4,5,6,7,8,9,0,112,33,4,5,556,67,66];
+let a = arr.filter((a) => {
+    return a < 50 
+})
+console.log(a)*/
+
+// array reduce method
+/*let arr = [1,2,3,4,5,6];
+let newarr = arr.reduce((h1,h2) =>{
+    return h1  + h2
+}) 
+console.log(newarr)*/
+    
